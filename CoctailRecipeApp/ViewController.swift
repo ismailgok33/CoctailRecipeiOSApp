8c9d87441457ba94246b7fd7d148f8748334f37e
İsmail <ismailgok33@gmail.com>
2022-03-06 01:20:51 -0500

    Initial Commit

---
+++ CoctailRecipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoctailRecipeApp
+//
+//  Created by İsmail on 6.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
